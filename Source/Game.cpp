8f536947900958a4c7755ed57bad6f004d783cc4
Vladimir <viperezf@upbc.edu.mx>
2014-09-18 13:27:38 -0700

    Pre - Actividad 3

--- Source/Game.cpp
+++ Source/Game.cpp
@@ -1,8 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "Game.h"
+#include <SDL.h>
 
 CGame::CGame(){
+	SDL_Surface *screen;
+	
+	if (SDL_Init( SDL_INIT_VIDEO )){
+		printf("Error %s ", SDL_GetError());
+		exit(EXIT_FAILURE);
+	}
+
+	screen = SDL_SetVideoMode( 640, 480, 24, SDL_SWSURFACE );
+	if (screen == NULL){
+		printf("Error %s ", SDL_GetError());
+		exit(EXIT_FAILURE);
+	}
+
+	SDL_WM_SetCaption( "Mi primer Juego", NULL );
+	
+	SDL_Flip(screen);
+
 	estado = 0;
 }
 
